0b1a0e1eaa39a26fb2e8107f7a4e50ad0e364ed4
sboukiou <sboukiou@student.1337.ma>
2024-11-15 09:44:32 +0100

    Adding prototype for the ft_printf() function in printf.h

--- printf.h
+++ printf.h
@@ -18,5 +18,6 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+int	ft_printf(const char *buffer, ...);
 
 #endif
