fadeda0169a951796233961e603597d8b7a4bcab
sboukiou <sboukiou@student.1337.ma>
2024-11-20 19:00:01 +0100

    Using return value from call_printer to jump to next char avoiding repeat of actual parsed format

--- printf.c
+++ printf.c
@@ -6,8 +6,10 @@ int	ft_printf(const char *buffer, ...)
 {
 	int	len;
 	int	index;
+	va_list	args_list;
 
 	len = 0;
+	va_start(args_list, buffer);
 	index = 0;
 	if (!buffer)
 	{
@@ -17,9 +19,7 @@ int	ft_printf(const char *buffer, ...)
 	while (buffer[index])
 	{
 		if (buffer[index] == '%')
-		{
-			ft_putstr_fd(_parse_format(buffer + index));
-		}
+			index += call_printer((char *)buffer + index, args_list);
 		else
 			ft_putchar_fd(buffer[index], 1);
 		index++;
