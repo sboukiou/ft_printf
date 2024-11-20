1aab7c5702954902c347f16bf63f48100c1d4d06
sboukiou <sboukiou@student.1337.ma>
2024-11-20 18:58:40 +0100

    main.c

--- main.c
+++ main.c
@@ -8,14 +8,7 @@
 
 int main(void)
 {
-	printf("|% d|\n", 1337);
-	printf("|% d|\n", -1337);
-	printf("|-----\n");
-	printf("|%-&x 8d|\n", -1337);
-	printf("%\n");
-	printf("|% -8d|\n", 1337);
-	printf("|%08d|\n", 1337);
-	printf("|%+8d|\n", 1337);
-	printf("|%-8d|\n", 1337);
-	printf("|%8d|\n", 1337);
+	ft_printf("|%+d|\n", 1337);
+	ft_printf("|%+d|\n", -1337);
+	ft_printf("|% s|\n", "String");
 }
