c2208f4ad15c7b1ec28daf44dfa3276fe757e9fc
sboukiou <sboukiou@student.1337.ma>
2024-11-20 19:01:17 +0100

    adding handler to call printers and parse according to the type passed

---
+++ handler.c
@@ -0,0 +1,36 @@
+#include "libft/libft.h"
+#include "printf.h"
+
+int	is_set(char c, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+
+int	call_printer(char *buffer, va_list args_list)
+{
+	int	index;
+
+	index = 1;
+	while (!is_set(buffer[index], TYPES))
+		index++;
+	if (buffer[index] == 'c')
+		print_char(va_arg(args_list, int));
+	else if (buffer[index] == 'd')
+	{
+		int value = va_arg(args_list, int);
+		print_int(value, get_flags(buffer));
+	}
+	else if (buffer[index] == 'i')
+		print_int(va_arg(args_list, int), get_flags(buffer));
+	return (index);
+}
