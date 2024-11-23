b7f758418cc5140c70142ad2f0bd451124ce9b2b
sboukiou <sboukiou@student.1337.ma>
2024-11-23 17:18:53 +0100

    Adding hex and address printing file

---
+++ print_hex_address.c
@@ -0,0 +1,29 @@
+#include "ft_printf.h"
+#include "ft_printf_bonus.h"
+
+
+int	print_address_p(void	*address, t_tokens *tokens)
+{
+	int	len;
+
+	len = 0;
+	(void)tokens;
+	print_address(address);
+	return (len);
+}
+
+int print_hex_x(unsigned int number, t_tokens *tokens)
+{
+	int	len;
+	len = 0;
+	len += print_hex(number, tokens->hash);
+	return (len);
+}
+
+int print_hex_X(unsigned int number, t_tokens *tokens)
+{
+	int	len;
+	len = 0;
+	len += print_hex_upper(number, tokens->hash);
+	return (len);
+}
