21aa2e83535f3d5e2c56feaaaefd98f98b0e9481
sboukiou <sboukiou@student.1337.ma>
2024-11-23 09:04:22 +0100

    tests main.c

--- tests/main.c
+++ tests/main.c
@@ -1,5 +1,6 @@
-#include "../printf.h"
+#include "../ft_printf.h"
 #include <stdio.h>
+#include <limits.h>
 #include <stdlib.h>
 
 
@@ -9,114 +10,207 @@
 
 int main() {
 
-    printf("|% 10i|\n", -0);
-    ft_printf("|% 10i|\n", -0);
+	int ret_t;
+	int	ret;
+
+    ret_t = printf("|% 10i|\n", -0);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|% 10i|\n", -0);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-    printf("|% 10i|\n", 0);
-    ft_printf("|% 10i|\n", 0);
+    ret_t = printf("|% 10i|\n", 0);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|% 10i|\n", 0);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-    printf("|% 10i|\n", -0);
-    ft_printf("|% 10i|\n", -0);
+    ret_t = printf("|% 10i|\n", -0);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|% 10i|\n", -0);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-    printf("|% 10i|\n", -1337);
-    ft_printf("|% 10i|\n", -1337);
+    ret_t = printf("|% 10i|\n", -1337);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|% 10i|\n", -1337);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
 
 
-    printf("|%i|\n", -1337);
-    ft_printf("|%i|\n", -1337);
+    ret_t = printf("|%i|\n", -1337);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|%i|\n", -1337);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-    printf("|% 10i|\n", 1337);
-    ft_printf("|% 10i|\n", 1337);
+    ret_t = printf("|% 10i|\n", 1337);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|% 10i|\n", 1337);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-    printf("|% 10i|\n", -13999999);
-    ft_printf("|% 10i|\n", -13999999);
+    ret_t = printf("|% 10i|\n", -13999999);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|% 10i|\n", -13999999);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-    printf("|% 10i|\n", -13999999);
-    ft_printf("|% 10i|\n", -13999999);
+    ret_t = printf("|% 10i|\n", -13999999);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|% 10i|\n", -13999999);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
-    printf("|% 10i|\n", 1);
-    ft_printf("|% 10i|\n", 1);
+    ret_t = printf("|% 10i|\n", 1);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|% 10i|\n", 1);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
 	//    NOTE: Testing with flag 0
 
 	printf("----- Testing with the 0 flag -----\n");
-    printf("|%010i|\n", -0);
-    ft_printf("|%010i|\n", -0);
+    ret_t = printf("|%010i|\n", -0);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|%010i|\n", -0);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-    printf("|%010i|\n", 0);
-    ft_printf("|%010i|\n", 0);
+    ret_t = printf("|%010i|\n", 0);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|%010i|\n", 0);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-    printf("|%010i|\n", -0);
-    ft_printf("|%010i|\n", -0);
+    ret_t = printf("|%010i|\n", -0);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|%010i|\n", -0);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-    printf("|%010i|\n", -1337);
-    ft_printf("|%010i|\n", -1337);
+    ret_t = printf("|%010i|\n", -1337);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|%010i|\n", -1337);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-    printf("|%i|\n", -1337);
-    ft_printf("|%i|\n", -1337);
+    ret_t = printf("|%i|\n", -1337);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|%i|\n", -1337);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-    printf("|%010i|\n", 1337);
-    ft_printf("|%010i|\n", 1337);
+    ret_t = printf("|%010i|\n", 1337);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|%010i|\n", 1337);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-    printf("|%010i|\n", -13999999);
-    ft_printf("|%010i|\n", -13999999);
+    ret_t = printf("|%010i|\n", -13999999);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|%010i|\n", -13999999);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-    printf("|%010i|\n", -13999999);
-    ft_printf("|%010i|\n", -13999999);
+    ret_t = printf("|%010i|\n", -13999999);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|%010i|\n", -13999999);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
-    printf("|%010i|\n", 1);
-    ft_printf("|%010i|\n", 1);
+    ret_t = printf("|%010i|\n", 1);
+	ft_printf("printf --> %d\n", ret_t);
+    ret = ft_printf("|%010i|\n", 1);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
 
 	// NOTE:    TESTING WITH + FLAG
 	printf("\n--- Testing with the + flag ---\n");
-	   printf("|%+10i|\n", -0);
-	   ft_printf("|%+10i|\n", -0);
+	   ret_t = printf("|%+10i|\n", -0);
+	ft_printf("printf --> %d\n", ret_t);
+	   ret = ft_printf("|%+10i|\n", -0);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-	   printf("|%+10i|\n", 0);
-	   ft_printf("|%+10i|\n", 0);
+	   ret_t = printf("|%+10i|\n", 0);
+	ft_printf("printf --> %d\n", ret_t);
+	   ret = ft_printf("|%+10i|\n", 0);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-	   printf("|%+10i|\n", -0);
-	   ft_printf("|%+10i|\n", -0);
+	   ret_t = printf("|%+10i|\n", -0);
+	ft_printf("printf --> %d\n", ret_t);
+	   ret = ft_printf("|%+10i|\n", -0);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-	   printf("|%+10i|\n", -1337);
-	   ft_printf("|%+10i|\n", -1337);
+	   ret_t = printf("|%+10i|\n", -1337);
+	ft_printf("printf --> %d\n", ret_t);
+	   ret = ft_printf("|%+10i|\n", -1337);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-	   printf("|%+10i|\n", 1337);
-	   ft_printf("|%+10i|\n", 1337);
+	   ret_t = printf("|%+10i|\n", 1337);
+	ft_printf("printf --> %d\n", ret_t);
+	   ret = ft_printf("|%+10i|\n", 1337);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-	   printf("|%+10i|\n", -13999999);
-	   ft_printf("|%+10i|\n", -13999999);
+	   ret_t = printf("|%+10i|\n", -13999999);
+	ft_printf("printf --> %d\n", ret_t);
+	   ret = ft_printf("|%+10i|\n", -13999999);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-	printf("|%+10i|\n", 1);
-	ft_printf("|%+10i|\n", 1);
+	ret_t = printf("|%+10i|\n", 1);
+	ft_printf("printf --> %d\n", ret_t);
+	ret = ft_printf("|%+10i|\n", 1);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
-	printf("|%+-12d|\n", 1);
-	ft_printf("|%+-12d|\n", 1);
+	ret_t = printf("|%+-12d|\n", 1);
+	ft_printf("printf --> %d\n", ret_t);
+	ret = ft_printf("|%+-12d|\n", 1);
+	ft_printf("ft_printf --> %d\n", ret);
+	printf("\n");
+
+	ret_t = printf("|%013d|\n", ULONG_MAX);
+	ft_printf("printf --> %d\n", ret_t);
+	ret = ft_printf("|%013d|\n", ULONG_MAX);
+	ft_printf("ft_printf --> %d\n", ret);
+	printf("\n");
+
+	ret_t = printf("|%015d|\n", INT_MIN);
+	ft_printf("printf --> %d\n", ret_t);
+	ret = ft_printf("|%015d|\n", INT_MIN);
+	ft_printf("ft_printf --> %d\n", ret);
+	printf("\n");
+
+
+	ret_t = printf("|%+-12d|\n", INT_MAX);
+	ft_printf("printf --> %d\n", ret_t);
+	ret = ft_printf("|%+-12d|\n", INT_MAX);
+	ft_printf("ft_printf --> %d\n", ret);
+	printf("\n");
+
+
+	ret_t = printf("|%013d|\n", LONG_MIN);
+	ft_printf("printf --> %d\n", ret_t);
+	ret = ft_printf("|%013d|\n", LONG_MIN);
+	ft_printf("ft_printf --> %d\n", ret);
+	printf("\n");
+
+	ret_t = printf("|%+-12d|\n", LONG_MAX);
+	ft_printf("printf --> %d\n", ret_t);
+	ret = ft_printf("|%+-12d|\n", LONG_MAX);
+	ft_printf("ft_printf --> %d\n", ret);
+	printf("\n");
+	
+	ret_t = printf("|%014d|\n", 0);
+	ft_printf("printf --> %d\n", ret_t);
+	ret = ft_printf("|%014d|\n", 0);
+	ft_printf("ft_printf --> %d\n", ret);
 	printf("\n");
 
     return 0;
