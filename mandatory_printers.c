0455aaf71a27f5af24f3d6d1801f4188cb20bc6d
sboukiou <sboukiou@student.1337.ma>
2024-11-23 17:20:47 +0100

    some fixes

--- mandatory_printers.c
+++ mandatory_printers.c
@@ -1,7 +1,7 @@
 #include "libft/libft.h"
 #include "ft_printf.h"
 
-static int	print_large_numbers(long int number)
+static int	print_large_number(long int number)
 {
 	int	len;
 	if (!number)
@@ -9,73 +9,10 @@ static int	print_large_numbers(long int number)
 	len = 0;
 	if (number < 10)
 		return (print_char(number + '0'));
-	len += print_large_numbers(number / 10);
+	len += print_large_number(number / 10);
 	len += print_char(number % 10 + '0');
 	return (len);
 }
-
-int	print_hex(unsigned int number, int hash)
-{
-
-	int	len;
-
-	len = 0;
-	if (!hash)
-		len += print_string("0x");
-	if (!number)
-		return (len += print_char('0'));
-	if (number < 16)
-		return (len += print_char(HEX_BASE[number]));
-	len += print_hex(number / 16, 1);
-	len += print_char(HEX_BASE[number % 16]);
-	return (len);
-}
-
-int	print_hex_upper(unsigned int number, int hash)
-{
-
-	int	len;
-
-	len = 0;
-	if (!hash)
-		len += print_string("0X");
-	if (!number)
-		return (print_char('0'));
-	if (number < 16)
-		return (print_char(HEX_BASE_UPPER[number]));
-	len += print_hex_upper(number / 16, 1);
-	len += print_char(HEX_BASE_UPPER[number % 16]);
-	return (len);
-}
-
-
-static int	print_hex_large(unsigned long number, int hash)
-{
-
-	int	len;
-
-	len = 0;
-	if (!hash)
-		len += print_string("0x");
-	if (!number)
-		return (len += print_char('0'));
-	if (number < 16)
-		return (len += print_char(HEX_BASE[number]));
-	len += print_hex_large(number / 16, 1);
-	len += print_char(HEX_BASE[number % 16]);
-	return (len);
-}
-
-int	print_address(void	*address)
-{
-	unsigned long	addr_value;
-
-	addr_value = (unsigned long)address;
-	if (!addr_value)
-		return (print_string("(nil)"));
-	return (print_hex_large(addr_value, 0));
-}
-
 
 int	ft_printf(const char *format, ...)
 {
@@ -98,9 +35,9 @@ int	ft_printf(const char *format, ...)
 			else if (format[index] == 's')
 				total_len += print_string(va_arg(args_list, char *));
 			else if (format[index] == 'd' || format[index] == 'i')
-				total_len += print_large_numbers(va_arg(args_list, int));
+				total_len += print_large_number(va_arg(args_list, int));
 			else if (format[index] == 'u')
-				total_len += print_large_numbers(va_arg(args_list, unsigned int));
+				total_len += print_large_number(va_arg(args_list, unsigned int));
 			else if (format[index] == 'p')
 				total_len += print_address(va_arg(args_list, void *));
 			else if (format[index] == 'X')
